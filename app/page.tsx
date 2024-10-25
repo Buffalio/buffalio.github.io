c131a2c26dec36a800651593e9b06ccef3b32a23
lphuctai <lphuctai@gmail.com>
2024-10-25 23:37:20 +0700

    Init

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,6 @@
-import client from "../tina/__generated__/client";
-import Layout from "../components/layout/layout";
-import ClientPage from "./[...filename]/client-page";
+import Page from "./[...filename]/page";
 import React from "react";
 
 export default async function HomePage() {
-  const data = await client.queries.page({
-    relativePath: `home.md`,
-  });
-
-  return (
-    <Layout rawPageData={data}>
-      <ClientPage {...data}></ClientPage>
-    </Layout>
-  );
+  return (<Page params={{ filename: ['home'] }} />);
 }
